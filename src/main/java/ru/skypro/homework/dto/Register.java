ccba548fbb6c74eef89c8cd2056564a68b069707
gitWestender <javaxwestender@gmail.com>
2024-12-05 19:35:52 +0300

    add validation params

--- src/main/java/ru/skypro/homework/dto/Register.java
+++ src/main/java/ru/skypro/homework/dto/Register.java
@@ -2,13 +2,38 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class Register {
-
+    @NotBlank(message = "Имя пользователя не может быть пустым")
+    @Size(min = 4, max = 32,
+            message = "Имя пользователя должно содержать от 4 до 32 символов")
+    @Email(message = "Email адрес должен быть в формате user@example.com")
     private String username;
+
+    @NotBlank(message = "Пароль не может быть пустым")
+    @Size(min = 8, max = 16,
+            message = "Длина пароля должна быть от 8 до 16 символов")
     private String password;
+
+    @NotBlank(message = "Имя должно быть указано")
+    @Size(min = 2, max = 16,
+            message = "Имя должно содержать от 2 до 16 символов")
     private String firstName;
+
+    @NotBlank(message = "Должна быть указана фамилия")
+    @Size(min = 2, max = 16,
+            message = "Фамилия должна содержать от 2 до 16 символов")
     private String lastName;
+
+    @NotBlank(message = "Должен быть указан номер телефона")
+    @Pattern(regexp = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}"
+            ,message = "Телефон должен начинаться с +7 и продолжаться 10 числами")
     private String phone;
+
     private Role role;
 }
