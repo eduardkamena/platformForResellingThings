92f752c4b4c3c05db85c64826d4fd1f5f406eeed
gitWestender <javaxwestender@gmail.com>
2024-12-09 21:47:38 +0300

    small fixes

--- src/main/java/ru/skypro/homework/repository/UserImageRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserImageRepository.java
@@ -1,7 +1,9 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.UserImage;
 
-public interface UserImageRepository extends JpaRepository<UserImage, Long> {
+@Repository
+public interface UserImageRepository extends JpaRepository<UserImage, Integer> {
 }
