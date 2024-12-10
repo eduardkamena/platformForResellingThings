19ff993dfd90432e285b40ed0e6e18283f6224ae
gitWestender <javaxwestender@gmail.com>
2024-12-10 20:23:59 +0300

    fix

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.comment.CommentDTO;
 import ru.skypro.homework.dto.comment.CommentsDTO;
 import ru.skypro.homework.dto.comment.CreateOrUpdateCommentDTO;
-import ru.skypro.homework.entity.Comment;
 import ru.skypro.homework.service.AdService;
 import ru.skypro.homework.service.CommentService;
 
@@ -25,8 +25,8 @@ public class CommentController {
 
     @PostMapping(path = "/{id}/comments")
     @Operation(description = "Добавление комментария к объявлению")
-    public ResponseEntity<Comment> createCommentToAdById(@PathVariable("id") Integer adId,
-                                                         @RequestBody CreateOrUpdateCommentDTO newComment) {
+    public ResponseEntity<CommentDTO> createCommentToAdById(@PathVariable("id") Integer adId,
+                                                            @RequestBody CreateOrUpdateCommentDTO newComment) {
         return null;
     }
 
