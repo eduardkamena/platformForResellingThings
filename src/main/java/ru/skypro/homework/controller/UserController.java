0acacb251763dd1a496e6c9e7dea3e876a5e13b9
gitWestender <javaxwestender@gmail.com>
2024-12-12 20:01:35 +0300

    add GET /me method

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,12 +1,12 @@
 package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.Register;
 import ru.skypro.homework.dto.user.UserDTO;
 import ru.skypro.homework.entity.User;
 import ru.skypro.homework.mapper.UserMapper;
