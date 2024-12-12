8e04664b7e97928361e8d934f2a7e479d7349e90
gitWestender <javaxwestender@gmail.com>
2024-12-12 19:54:45 +0300

    refactor initialization size of few columns

--- src/main/resources/liquibase/migration/user_table.sql
+++ src/main/resources/liquibase/migration/user_table.sql
@@ -5,10 +5,10 @@ CREATE TABLE IF NOT EXISTS t_user
 (
     id          SERIAL             NOT NULL,
     c_username  VARCHAR(32) UNIQUE NOT NULL,
-    c_password  VARCHAR(16)        NOT NULL,
+    c_password  VARCHAR(255)        NOT NULL,
     c_firstname VARCHAR(16)        NOT NULL,
     c_lastname  VARCHAR(16)        NOT NULL,
-    c_phone     VARCHAR(12) UNIQUE NOT NULL,
+    c_phone     VARCHAR(18) UNIQUE NOT NULL,
     c_role      VARCHAR(32)        NOT NULL,
     c_image     INT,
 
