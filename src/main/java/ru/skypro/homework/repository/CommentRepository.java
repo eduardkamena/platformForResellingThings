329e205d2278aeaccf42dcdc13b50b60d828e55c
Ed <eduard@kamenskikh.me>
2024-12-25 06:29:22 +0300

    Добавил методы в Repositories

--- src/main/java/ru/skypro/homework/repository/CommentRepository.java
+++ src/main/java/ru/skypro/homework/repository/CommentRepository.java
@@ -1,9 +1,19 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.Comment;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    @Query(value = "SELECT * FROM comments WHERE ad_id = :ad_id", nativeQuery = true)
+    List<Comment> findByAdId(@Param("ad_id") Integer adId);
+
+    Comment findFirstByText(String text);
+
 }
--- src/main/java/ru/skypro/homework/repository/ImageRepository.java
+++ src/main/java/ru/skypro/homework/repository/ImageRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.Image;
 
+import java.util.Optional;
+
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+    @Override
+    Optional<Image> findById(Integer id);
+
 }
