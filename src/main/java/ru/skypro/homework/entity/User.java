400306acd50e5ba1eaf6ba63643b00223d653249
Ed <eduard@kamenskikh.me>
2024-12-29 01:42:54 +0300

    Рефакторинг

--- src/main/java/ru/skypro/homework/entity/User.java
+++ src/main/java/ru/skypro/homework/entity/User.java
@@ -19,7 +19,7 @@ public class User{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "username")
+    @Column(name = "email")
     private String email;
 
     @Column(name = "password")
