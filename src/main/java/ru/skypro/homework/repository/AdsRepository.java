4c0b0d3d37c911a0f556e427558d40d9c015d55d
Ed <eduard@kamenskikh.me>
2025-01-05 00:46:02 +0300

    Добавлено описание JavaDoc репозиториев

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsRepository.java
@@ -7,9 +7,25 @@ import ru.skypro.homework.entity.UserEntity;
 
 import java.util.List;
 
+/**
+ * Репозиторий для работы с сущностями объявлений (AdEntity).
+ * <p>
+ * Этот интерфейс предоставляет методы для выполнения операций с базой данных,
+ * таких как поиск объявлений по автору.
+ * </p>
+ *
+ * @see Repository
+ * @see JpaRepository
+ */
 @Repository
 public interface AdsRepository extends JpaRepository<AdEntity, Integer> {
 
+    /**
+     * Находит все объявления, созданные указанным автором.
+     *
+     * @param author сущность пользователя, являющегося автором объявлений.
+     * @return список объявлений, созданных указанным автором.
+     */
     List<AdEntity> findByAuthor(UserEntity author);
 
 }
--- src/main/java/ru/skypro/homework/repository/ImageRepository.java
+++ src/main/java/ru/skypro/homework/repository/ImageRepository.java
@@ -4,9 +4,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.ImageEntity;
 
+/**
+ * Репозиторий для работы с сущностями изображений (ImageEntity).
+ * <p>
+ * Этот интерфейс предоставляет методы для выполнения операций с базой данных,
+ * таких как удаление изображений по пути к файлу.
+ * </p>
+ *
+ * @see Repository
+ * @see JpaRepository
+ */
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
 
+    /**
+     * Удаляет изображение по указанному пути к файлу.
+     *
+     * @param filePath путь к файлу изображения.
+     */
     void deleteByFilePath(String filePath);
 
 }
