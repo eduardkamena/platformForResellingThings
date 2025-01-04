829c9e39e0028b7d1eb1ceb1d27524bb0cfac7f4
Ed <eduard@kamenskikh.me>
2025-01-05 01:02:02 +0300

    Добавлено описание JavaDoc классов пакета security

--- src/main/java/ru/skypro/homework/security/MyUserDetailsService.java
+++ src/main/java/ru/skypro/homework/security/MyUserDetailsService.java
@@ -9,6 +9,18 @@ import ru.skypro.homework.exception.UserNotFoundException;
 import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.repository.UserRepository;
 
+/**
+ * Сервис для загрузки данных пользователя в Spring Security.
+ * <p>
+ * Этот класс реализует интерфейс {@link UserDetailsService} и используется для поиска пользователя
+ * по email и преобразования его данных в объект {@link MyUserDetails}, необходимый для аутентификации.
+ * </p>
+ *
+ * @see Service
+ * @see RequiredArgsConstructor
+ * @see UserDetailsService
+ * @see MyUserDetails
+ */
 @Service
 @RequiredArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
@@ -17,6 +29,13 @@ public class MyUserDetailsService implements UserDetailsService {
     private final MyUserDetails myUserDetails;
     private final UserMapper userMapper;
 
+    /**
+     * Загружает данные пользователя по email.
+     *
+     * @param email email пользователя.
+     * @return объект {@link MyUserDetails}, содержащий данные пользователя.
+     * @throws UsernameNotFoundException если пользователь с указанным email не найден.
+     */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         MyUserDetailsDTO myUserDetailsDto = userRepository.findByEmail(email)
