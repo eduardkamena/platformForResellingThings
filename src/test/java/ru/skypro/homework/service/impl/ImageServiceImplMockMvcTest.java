39722cfac0aa9e960269bb472582eaf526e9cbdf
Ed <eduard@kamenskikh.me>
2025-01-23 00:14:41 +0300

    Рефакторинг ImageServiceImplMockMvcTest.java

--- src/test/java/ru/skypro/homework/service/impl/ImageServiceImplMockMvcTest.java
+++ src/test/java/ru/skypro/homework/service/impl/ImageServiceImplMockMvcTest.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -32,6 +33,9 @@ public class ImageServiceImplMockMvcTest {
 
     private MultipartFile testFile;
     private ImageEntity testImageEntity;
+
+    // Создаем путь к тестируемым файлам изображений
+    private final String imageDir = "src/main/resources/images";
 
     @BeforeEach
     public void setUp() throws NoSuchFieldException, IllegalAccessException, IOException {
@@ -52,7 +56,7 @@ public class ImageServiceImplMockMvcTest {
         // Устанавливаем значение imageDir через рефлексию
         Field imageDirField = ImageServiceImpl.class.getDeclaredField("imageDir");
         imageDirField.setAccessible(true); // Делаем поле доступным
-        imageDirField.set(imageService, "testImageDir"); // Устанавливаем значение
+        imageDirField.set(imageService, imageDir); // Устанавливаем значение
     }
 
     @Test
@@ -65,14 +69,26 @@ public class ImageServiceImplMockMvcTest {
         // then
         assertNotNull(resultPath);
         assertTrue(resultPath.contains("testDir/image/"));
+
+        // Проверяем, что файл был создан на диске
+        String fileName = resultPath.substring(resultPath.lastIndexOf('/') + 1);
+        Path filePath = Paths.get(imageDir, fileName);
+        assertTrue(Files.exists(filePath));
+
+        // Проверяем, что метод save был вызван
         verify(imageRepository, times(1)).save(any(ImageEntity.class));
+
+        // Удаляем файл после теста
+        Files.deleteIfExists(filePath);
+        assertFalse(Files.exists(filePath));
+
     }
 
     @Test
     public void shouldGetImageAndReturnImageBytes() throws IOException {
         // given
         String imageName = "testImage.png";
-        Path imagePath = Path.of("testImageDir", imageName);
+        Path imagePath = Path.of(imageDir, imageName);
         Files.createDirectories(imagePath.getParent());
         Files.write(imagePath, testFile.getBytes());
 
@@ -104,11 +120,11 @@ public class ImageServiceImplMockMvcTest {
         String nonExistentImagePath = "nonExistentPath/testImage.png";
 
         // Удаляем файл, если он существует
-        Path filePath = Path.of("testImageDir", nonExistentImagePath);
+        Path filePath = Path.of(imageDir, nonExistentImagePath);
         try {
             Files.deleteIfExists(filePath);
         } catch (IOException e) {
-            fail("Failed to delete file during test setup: " + e.getMessage());
+            fail("Failed to delete file: " + e.getMessage());
         }
 
         // then
